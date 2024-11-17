5b458bf626c5066d21a13f3050f5a10273f256be
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2024-11-17 15:10:44 +0500

    logo correction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,8 +83,9 @@ export function Component() {
         <Navbar>
           <NavbarBrand>
             <img
-              src="https://s3-alpha-sig.figma.com/img/2727/769b/a74736d502746301ed573ed8940fc322?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V-KgMF65bBpedJXfxnEh5Re44eKl0ptjo1vHE0H2caKlZSKxSiipCgF9xMEBLT8rrCzA4qLXt6vUNroksYtvS2SrZ4PFU1TG6OtrH5UjO~XMt8JFfNVgS~fQzJiRvpPn7hvXPyXfdgVMgVfyKtFgkwlDXg7B9QBgKybRWGg8BTCd5RlnYtNW57N4FcL3m9o64gdFFannJlge4WJFhm1UKBfZ3js-VcQb6DhAmaNCdg9XL8cr0cbT68Y6XV6g1S3IZcJUjmWtbQhteIxuhhMdTsAiglZLBo1WRY6tIoLAz3Sjq8xJxydvHnf76FX-HmucGkl6FgUjLxbUYIiwEPPCNw__"
-              className="lg:h-16 h-16"
+              src="/images/logo.png"
+
+              className="lg:h-8 h-8"
               alt="Flowbite React Logo"
             />
 
--- src/components/PageTop.jsx
+++ src/components/PageTop.jsx
@@ -10,10 +10,10 @@ function PageTop({heading, linkName, to}) {
           alt=""
         />
         
-        <div className="absolute w-full h-full flex flex-col justify-center items-center">
+        <div className="absolute w-full h-full flex flex-col justify-center items-center gap-4">
         <img
-              src="https://s3-alpha-sig.figma.com/img/2727/769b/a74736d502746301ed573ed8940fc322?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V-KgMF65bBpedJXfxnEh5Re44eKl0ptjo1vHE0H2caKlZSKxSiipCgF9xMEBLT8rrCzA4qLXt6vUNroksYtvS2SrZ4PFU1TG6OtrH5UjO~XMt8JFfNVgS~fQzJiRvpPn7hvXPyXfdgVMgVfyKtFgkwlDXg7B9QBgKybRWGg8BTCd5RlnYtNW57N4FcL3m9o64gdFFannJlge4WJFhm1UKBfZ3js-VcQb6DhAmaNCdg9XL8cr0cbT68Y6XV6g1S3IZcJUjmWtbQhteIxuhhMdTsAiglZLBo1WRY6tIoLAz3Sjq8xJxydvHnf76FX-HmucGkl6FgUjLxbUYIiwEPPCNw__"
-              className="lg:w-16 md:w-12 w-8 transition translate-y-4 "
+              src="/images/logo.png"
+              className="lg:w-12 md:w-18 w-6 transition translate-y-4 "
               alt="Flowbite React Logo"
             />
           <h1 className=" text-center lg:text-4xl md:text-2xl text-xl font-semibold mb-4">                       
